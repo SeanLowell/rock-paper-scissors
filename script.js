8fadcd0cd56a94a7ddb0e3c4ef2a5184bff12b4a
Sean Lowell <seanlowell27@gmail.com>
2022-11-25 00:21:10 +0800

    create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+//randomly pick a choice for the computer
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"]
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    console.log(computerChoice);
+}
+getComputerChoice();
